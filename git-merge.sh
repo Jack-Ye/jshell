9c93d318cc11a6944144105092521f1a357bec7c
yepengwei <yepengwei@didichuxing.com>
2018-10-20 11:01:46 +0800

    feat:添加说明

--- git-merge.sh
+++ git-merge.sh
@@ -3,16 +3,27 @@
 
 br=`git branch | grep "*"`
 current=${br/* /}
-echo ${current}
+
+echo "====合并master分支到${current}分支===="
 
 git pull origin master
 
+echo "====推送${current}分支到远程仓库===="
+
 git push origin ${current}
+
+echo "====切换到dev分支===="
 
 git checkout dev
 
+echo "====合并${current}分支到dev分支===="
+
 git merge ${current}
 
+echo "====推送dev分支到远程仓库===="
+
 git push origin dev
+
+echo "====切换到${current}分支===="
 
 git checkout ${current}
