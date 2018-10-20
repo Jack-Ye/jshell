7572d3808a8a9969fbdf5ebff119feeecdaeed9e
yepengwei <yepengwei@didichuxing.com>
2018-10-20 11:05:18 +0800

    feat:支持指定远程

--- git-merge.sh
+++ git-merge.sh
@@ -3,6 +3,7 @@
 
 br=`git branch | grep "*"`
 current=${br/* /}
+target=$0
 
 echo "====1. 合并master分支到${current}分支===="
 
@@ -12,17 +13,17 @@ echo "\n\n====2. 推送${current}分支到远程仓库===="
 
 git push origin ${current}
 
-echo "\n\n====3. 切换到dev分支===="
+echo "\n\n====3. 切换到${target}分支===="
 
-git checkout dev
+git checkout ${target}
 
-echo "\n\n====4. 合并${current}分支到dev分支===="
+echo "\n\n====4. 合并${current}分支到${target}分支===="
 
 git merge ${current}
 
-echo "\n\n====5. 推送dev分支到远程仓库===="
+echo "\n\n====5. 推送${target}分支到远程仓库===="
 
-git push origin dev
+git push origin ${target}
 
 echo "\n\n====6. 切换到${current}分支===="
 
