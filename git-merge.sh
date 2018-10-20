874123c5b7634ef1eec42f3e3c0f5dc4337bc908
yepengwei <yepengwei@didichuxing.com>
2018-10-20 11:03:26 +0800

    feat:优化文案

--- git-merge.sh
+++ git-merge.sh
@@ -4,26 +4,26 @@
 br=`git branch | grep "*"`
 current=${br/* /}
 
-echo "====合并master分支到${current}分支===="
+echo "====1. 合并master分支到${current}分支===="
 
 git pull origin master
 
-echo "====推送${current}分支到远程仓库===="
+echo "\n\n====2. 推送${current}分支到远程仓库===="
 
 git push origin ${current}
 
-echo "====切换到dev分支===="
+echo "\n\n====3. 切换到dev分支===="
 
 git checkout dev
 
-echo "====合并${current}分支到dev分支===="
+echo "\n\n====4. 合并${current}分支到dev分支===="
 
 git merge ${current}
 
-echo "====推送dev分支到远程仓库===="
+echo "\n\n====5. 推送dev分支到远程仓库===="
 
 git push origin dev
 
-echo "====切换到${current}分支===="
+echo "\n\n====6. 切换到${current}分支===="
 
 git checkout ${current}
