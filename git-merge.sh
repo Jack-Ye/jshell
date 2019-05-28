270c3e9499f4a3b510bea2457e66853728ec1e3d
ye_pw <ye_pw@ypshengxian.com>
2019-05-28 10:58:23 +0800

    feat:删除目标分钟

--- git-merge.sh
+++ git-merge.sh
@@ -13,22 +13,26 @@ echo "\n\n====2. 推送${current}分支到远程仓库===="
 
 git push origin ${current}
 
-echo "\n\n====3. 切换到${target}分支===="
+echo "\n\n====3. 删除${target}分支===="
 
-git checkout ${target}
+git branch -d ${target}
 
-echo "\n\n====4. 更新${target}分支===="
+echo "\n\n====4. 切换到${target}分支===="
+
+git checkout -b ${target}
+
+echo "\n\n====5. 更新${target}分支===="
 
 git pull origin ${target}
 
-echo "\n\n====5. 合并${current}分支到${target}分支===="
+echo "\n\n====6. 合并${current}分支到${target}分支===="
 
 git merge ${current}
 
-echo "\n\n====6. 推送${target}分支到远程仓库===="
+echo "\n\n====7. 推送${target}分支到远程仓库===="
 
 git push origin ${target}
 
-echo "\n\n====7. 切换到${current}分支===="
+echo "\n\n====8. 切换到${current}分支===="
 
 git checkout ${current}
